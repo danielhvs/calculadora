06c2c69e62a72af2ddcc41a24e7844c8917b132f
danielhvs <danielhvs70@gmail.com>
2017-05-23 00:21:12 -0300

    calculando melhor mas falta guardar estado dos input texts

--- mobile-calculadora/src/mobile_calculadora/android/core.cljs
+++ mobile-calculadora/src/mobile_calculadora/android/core.cljs
@@ -10,6 +10,7 @@
 (def text (r/adapt-react-class (.-Text ReactNative)))
 (def text-input (r/adapt-react-class (.-TextInput ReactNative)))
 (def view (r/adapt-react-class (.-View ReactNative)))
+(def scroll-view (r/adapt-react-class (.-ScrollView ReactNative)))
 (def image (r/adapt-react-class (.-Image ReactNative)))
 (def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
 
@@ -18,13 +19,13 @@
 (defn alert [title]
       (.alert (.-Alert ReactNative) title))
 
-(defonce moedas (atom {:peso {:nome "PES" :preco-cambio 0.33 :preco-loja 2 :carteira 10 :reais-carteira 0} 
-                       :dolar {:nome "USD" :preco-cambio 3.40 :preco-loja 25 :carteira 5 :reais-carteira 0}
-                       :euro {:nome "EUR" :preco-cambio 4.10 :preco-loja 23 :carteira 0 :reais-carteira 0}
-                       :real {:nome "R$" :preco-cambio 1 :preco-loja 14 :carteira 0 :reais-carteira 0}}))
-(def chaves [:peso :dolar :euro])
+(defonce moedas (atom {:peso {:nome "PES" :preco-cambio 0.33 :preco-loja 2 :carteira 10 :reais-carteira 3.30} 
+                       :dolar {:nome "USD" :preco-cambio 3.40 :preco-loja 25 :carteira 5 :reais-carteira 17}
+                       :euro {:nome "EUR" :preco-cambio 4.10 :preco-loja 23 :carteira 6 :reais-carteira 16.60}
+                       :real {:nome "R$" :preco-cambio 1 :preco-loja 14 :carteira 3 :reais-carteira 3}}))
+(def chaves [:peso :dolar :euro :real])
 
-(def border-width 0)
+(def border-width 2)
 (def styles {
              :input {:font-size 8 :flex 1} 
              :titulo {:color "blue" :font-size 10 :text-align "center" :text-align-vertical "center"} 
@@ -33,10 +34,9 @@
              :borda-blue {:border-color "blue" :border-width border-width} 
              :borda-green {:border-color "green" :border-width border-width} 
              :view-titulo {:flex 1 :align-items "center" :justify-content "center"} 
-             :lista-esquerda {:flex 12 :align-items "stretch" :justify-content "flex-start"} 
+             :lista-esquerda {:flex 3 :align-items "stretch" :justify-content "flex-start"} 
              :fila {:flex 1 :flex-direction "row" :align-items "center" :justify-content "flex-start"} 
              :input-preco {:flex-direction "row" :align-items "center" :justify-content "space-around"}
-             :label-container {:flex-direction "row" :align-items "center" :justify-content "flex-end" :flex 1 }
              :label {:align-items "flex-start"}
              :texto-pequeno {:font-size 8}
 })
@@ -88,22 +88,31 @@
 (defn app-root []
   (let [greeting (subscribe [:get-greeting])]
     (fn []
-      [view (estilos [:estilo-tudo :borda-red])
-       [view (estilos [:view-titulo :borda-green]) 
-        (titulo "Preco de cambio")]
-       [view (estilos [:lista-esquerda :borda-green])
-        (for [chave chaves]
-          (input-preco (str "Comprei cada " (:nome (chave @moedas)) " por R$ " ) [text-input (estilos [:input]) (str (:preco-cambio (chave @moedas)))]))]
-       [view (estilos [:view-titulo :borda-green]) 
-        (titulo "Dinheiro na carteira")]
-       [view (estilos [:lista-esquerda :borda-green])
-        (for [chave chaves]
-          (input-preco (str "" (:nome (chave @moedas)) ": " ) [text-input (estilos [:input]) (str (:carteira (chave @moedas)))]))]
-       [view (estilos [:view-titulo :borda-green]) 
-        (titulo "Preco na loja")]
-       [view (estilos [:lista-esquerda :borda-green])
-        (for [chave chaves]
-          (input-preco (str "" (:nome (chave @moedas)) ": " ) [text-input (estilos [:input]) (str (:preco-loja (chave @moedas)))]))]
+      [scroll-view (estilos [:estilo-tudo :borda-red])
+       [view (estilos [:borda-red])
+        [view (estilos [:view-titulo :borda-green]) 
+         (titulo "Preco de cambio")]
+        [view (estilos [:lista-esquerda :borda-green])
+         (for [chave chaves]
+           (input-preco (str "Comprei cada " (:nome (chave @moedas)) " por R$ " ) [text-input (estilos [:input]) (str (:preco-cambio (chave @moedas)))]))]]
+       [view (estilos [:borda-red])
+        [view (estilos [:view-titulo :borda-blue]) 
+         (titulo "Dinheiro na carteira")]
+        [view (estilos [:lista-esquerda :borda-green])
+         (for [chave chaves]
+           (input-preco (str "" (:nome (chave @moedas)) ": " ) [text-input (estilos [:input]) (str (:carteira (chave @moedas)))]))]]
+       [view (estilos [:borda-red])
+        [view (estilos [:view-titulo :borda-green]) 
+         (titulo "Preco na loja")]
+        [view (estilos [:lista-esquerda :borda-green])
+         (for [chave chaves]
+           (input-preco (str "" (:nome (chave @moedas)) ": " ) [text-input (estilos [:input]) (str (:preco-loja (chave @moedas)))]))]]
+       [view (estilos [:borda-red])
+        [view (estilos [:view-titulo :borda-green]) 
+         (titulo "Melhor pagar com")]
+        [view (estilos [:lista-esquerda :borda-green])
+         (for [melhor (calcula-melhor)]
+           (input-preco (str "Melhor: " (:nome melhor) ": " ) [text (estilos [:text-pequeno]) (:preco-loja melhor)]))]]      
        ])))
 
 (defn init []
