6e2fc14ececc1b6641c613ef3b66190f45ac1ffd
danielhvs <danielhvs70@gmail.com>
2017-05-23 01:01:13 -0300

    melhorias layout.

--- mobile-calculadora/src/mobile_calculadora/android/core.cljs
+++ mobile-calculadora/src/mobile_calculadora/android/core.cljs
@@ -21,24 +21,23 @@
 
 (defonce moedas (atom {:peso {:nome "PES" :preco-cambio 0.33 :preco-loja 2 :carteira 10 :reais-carteira 3.30} 
                        :dolar {:nome "USD" :preco-cambio 3.40 :preco-loja 25 :carteira 5 :reais-carteira 17}
-                       :euro {:nome "EUR" :preco-cambio 4.10 :preco-loja 23 :carteira 6 :reais-carteira 16.60}
+                       :euro {:nome "EUR" :preco-cambio 4.10 :preco-loja 3 :carteira 6 :reais-carteira 16.60}
                        :real {:nome "R$" :preco-cambio 1 :preco-loja 14 :carteira 3 :reais-carteira 3}}))
-(def chaves [:peso :dolar :euro :real])
+(def chaves [:peso :dolar :euro])
 
-(def border-width 2)
+(def border-width 0)
 (def styles {
-             :input {:font-size 8 :flex 1} 
-             :titulo {:color "blue" :font-size 10 :text-align "center" :text-align-vertical "center"} 
+             :input {:font-size 16 :flex 1} 
+             :titulo {:color "blue" :font-size 16 :text-align "center" :text-align-vertical "center"} 
              :estilo-tudo {:flex 1}
              :borda-red {:border-color "red" :border-width border-width} 
              :borda-blue {:border-color "blue" :border-width border-width} 
              :borda-green {:border-color "green" :border-width border-width} 
-             :view-titulo {:flex 1 :align-items "center" :justify-content "center"} 
-             :lista-esquerda {:flex 3 :align-items "stretch" :justify-content "flex-start"} 
-             :fila {:flex 1 :flex-direction "row" :align-items "center" :justify-content "flex-start"} 
-             :input-preco {:flex-direction "row" :align-items "center" :justify-content "space-around"}
-             :label {:align-items "flex-start"}
-             :texto-pequeno {:font-size 8}
+             :view-titulo {:flex 3 :align-items "center" :justify-content "center"} 
+             :lista-esquerda {:flex 1 :justify-content "flex-start"} 
+             :fila {:flex 1 :flex-direction "row"} 
+             :label {:align-items "flex-end" :justify-content "space-around"}
+             :texto-pequeno {:font-size 12}
 })
 
 (defn estilos [chaves]
@@ -100,19 +99,19 @@
          (titulo "Dinheiro na carteira")]
         [view (estilos [:lista-esquerda :borda-green])
          (for [chave chaves]
-           (input-preco (str "" (:nome (chave @moedas)) ": " ) [text-input (estilos [:input]) (str (:carteira (chave @moedas)))]))]]
+           (input-preco (str (:nome (chave @moedas)) " " ) [text-input (estilos [:input]) (str (:carteira (chave @moedas)))]))]]
        [view (estilos [:borda-red])
         [view (estilos [:view-titulo :borda-green]) 
          (titulo "Preco na loja")]
         [view (estilos [:lista-esquerda :borda-green])
          (for [chave chaves]
-           (input-preco (str "" (:nome (chave @moedas)) ": " ) [text-input (estilos [:input]) (str (:preco-loja (chave @moedas)))]))]]
+           (input-preco (str (:nome (chave @moedas)) " " ) [text-input (estilos [:input]) (str (:preco-loja (chave @moedas)))]))]]
        [view (estilos [:borda-red])
         [view (estilos [:view-titulo :borda-green]) 
          (titulo "Melhor pagar com")]
         [view (estilos [:lista-esquerda :borda-green])
          (for [melhor (calcula-melhor)]
-           (input-preco (str "Melhor: " (:nome melhor) ": " ) [text (estilos [:text-pequeno]) (:preco-loja melhor)]))]]      
+           (input-preco (str (:nome melhor) " " (:preco-loja melhor) ) [text "" ]))]]      
        ])))
 
 (defn init []
